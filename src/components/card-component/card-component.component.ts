3ad6a26e922dfa54dac9857521761ab4107fc412
pmarchenko <pmarchenko@solit-clouds.ru>
2023-07-13 17:24:38 +0300

    bug fix

--- src/components/card-component/card-component.component.ts
+++ src/components/card-component/card-component.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {GameCard} from "../../app/app.service";
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-card-component',
@@ -10,13 +11,18 @@ export class CardComponentComponent implements OnInit {
 
   @Input()
   public data!: GameCard;
+  public safeDescription!: SafeHtml;
 
 
-  constructor() {
+  constructor(
+    private sanitizer: DomSanitizer
+  ) {
   }
 
 
   ngOnInit(): void {
+    let descriptionHtml = this.data.description.replace(/([\n\u2028])/g, '<br/>');
+    this.safeDescription = this.sanitizer.bypassSecurityTrustHtml(descriptionHtml);
   }
 
 }
